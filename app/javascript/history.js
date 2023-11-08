5ea1ff002b0192eba7d094a55cb3d4f8483c847a
aryu-shan <145306193+aryu-shan@users.noreply.github.com>
2023-11-08 16:13:53 +0900

    問題文連携

--- app/javascript/history.js
+++ app/javascript/history.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
-  var wordlist = ["aaieba kouiu", "waraukadoniha hukukitaru", "umanomimininenbutu"];
-  var wordlistJapanese = ["ああ言えばこう言う", "笑う門には福来たる", "馬の耳に念仏"];
+  var questions = [];  // 問題文の配列
+  var translations = [];  // 日本語訳の配列
+  var currentQuestionIndex = 0;
   var time_limit = 60;
   var readytime = 3;
   var score;
@@ -8,16 +9,52 @@ document.addEventListener("DOMContentLoaded", function() {
   var mistake;
   var char_num = 0;
   var word_char;
-  var random;
-  var newline;
   var wordElement = document.getElementById("word");
   var japaneseElement = document.getElementById("japanese");
   var countElement = document.getElementById("count");
   var scoredisElement = document.getElementById("scoredis");
   var startButton = document.getElementById("start_button");
-
-  var missaudio = new Audio("_sound_anime_mp3_incorrect1.mp3");
-  var correctaudio = new Audio("_sound_battle_mp3_handgun-out-bullets1.mp3");
+  var map = { 'あ': ['a'], 'い': ['i', 'yi'], 'う': ['u', 'wu'], 'え': ['e'], 'お': ['o'],
+  'か': ['ka', 'ca'], 'き': ['ki'], 'く': ['ku', 'cu', 'qu'], 'け': ['ke'], 'こ': ['ko', 'co'],
+  'さ': ['sa'], 'し': ['si', 'shi', 'ci'], 'す': ['su'], 'せ': ['se', 'ce'], 'そ': ['so'],
+  'た': ['ta'], 'ち': ['ti', 'chi'], 'つ': ['tu', 'tsu'], 'て': ['te'], 'と': ['to'],
+  'な': ['na'], 'に': ['ni'], 'ぬ': ['nu'], 'ね': ['ne'], 'の': ['no'],
+  'は': ['ha'], 'ひ': ['hi'], 'ふ': ['fu', 'hu'], 'へ': ['he'], 'ほ': ['ho'],
+  'ま': ['ma'], 'み': ['mi'], 'む': ['mu'], 'め': ['me'], 'も': ['mo'],
+  'や': ['ya'], 'ゆ': ['yu'], 'よ': ['yo'],
+  'ら': ['ra'], 'り': ['ri'], 'る': ['ru'], 'れ': ['re'], 'ろ': ['ro'],
+  'わ': ['wa'], 'ゐ': ['wyi'], 'ゑ': ['wye'], 'を': ['wo'], 'ん': ['nn', 'xn', 'n'],
+  'が': ['ga'], 'ぎ': ['gi'], 'ぐ': ['gu'], 'げ': ['ge'], 'ご': ['go'],
+  'ざ': ['za'], 'じ': ['ji', 'zi'], 'ず': ['zu'], 'ぜ': ['ze'], 'ぞ': ['zo'],
+  'だ': ['da'], 'ぢ': ['di'], 'づ': ['du'], 'で': ['de'], 'ど': ['do'],
+  'ば': ['ba'], 'び': ['bi'], 'ぶ': ['bu'], 'べ': ['be'], 'ぼ': ['bo'],
+  'ぱ': ['pa'], 'ぴ': ['pi'], 'ぷ': ['pu'], 'ぺ': ['pe'], 'ぽ': ['po'],
+  'うぁ': ['wha'], 'うぃ': ['whi'], 'うぇ': ['whe'], 'うぉ': ['who'],
+  'きゃ': ['kya'], 'きぃ': ['kyi'], 'きゅ': ['kyu'], 'きぇ': ['kye'], 'きょ': ['kyo'],
+  'くぁ': ['qa', 'qwa'], 'くぃ': ['qi', 'qwi'], 'くぇ': ['qe', 'qwe'], 'くぉ': ['qo', 'qwo'], 'くゃ': ['qya'], 'くゅ': ['qyu'], 'くょ': ['qyo'],
+  'しゃ': ['sya', 'sha'], 'しぃ': ['syi'], 'しゅ': ['syu', 'shu'], 'しぇ': ['sye', 'she'], 'しょ': ['syo', 'sho'],
+  'つぁ': ['tsa'], 'つぃ': ['tsi'], 'つぇ': ['tse'], 'つぉ': ['tso'],
+  'ちゃ': ['tya', 'cha'], 'ちぃ': ['tyi'], 'ちゅ': ['tyu', 'chu'], 'ちぇ': ['tye', 'che'], 'ちょ': ['tyo', 'cho'],
+  'てゃ': ['tha'], 'てぃ': ['thi'], 'てゅ': ['thu'], 'てぇ': ['the'], 'てょ': ['tho'],
+  'とぁ': ['twa'], 'とぃ': ['twi'], 'とぅ': ['twu'], 'とぇ': ['twe'], 'とぉ': ['two'],
+  'ひゃ': ['hya'], 'ひぃ': ['hyi'], 'ひゅ': ['hyu'], 'ひぇ': ['hye'], 'ひょ': ['hyo'],
+  'ふぁ': ['fa'], 'ふぃ': ['fi'], 'ふぇ': ['fe'], 'ふぉ': ['fo'],
+  'にゃ': ['nya'], 'にぃ': ['nyi'], 'にゅ': ['nyu'], 'にぇ': ['nye'], 'にょ': ['nyo'],
+  'みゃ': ['mya'], 'みぃ': ['myi'], 'みゅ': ['myu'], 'みぇ': ['mye'], 'みょ': ['myo'],
+  'りゃ': ['rya'], 'りぃ': ['ryi'], 'りゅ': ['ryu'], 'りぇ': ['rye'], 'りょ': ['ryo'],
+  'ヴぁ': ['va'], 'ヴぃ': ['vi'], 'ヴ': ['vu'], 'ヴぇ': ['ve'], 'ヴぉ': ['vo'],
+  'ぎゃ': ['gya'], 'ぎぃ': ['gyi'], 'ぎゅ': ['gyu'], 'ぎぇ': ['gye'], 'ぎょ': ['gyo'],
+  'ぐぁ': ['gwa'], 'ぐぃ': ['gwi'], 'ぐぅ': ['gwu'], 'ぐぇ': ['gwe'], 'ぐぉ': ['gwo'],
+  'じゃ': ['ja', 'zya'], 'じぃ': ['jyi', 'zyi'], 'じゅ': ['ju', 'zyu'], 'じぇ': ['je', 'zye'], 'じょ': ['jo', 'zyo'],
+  'でゃ': ['dha'], 'でぃ': ['dhi'], 'でゅ': ['dhu'], 'でぇ': ['dhe'], 'でょ': ['dho'],
+  'ぢゃ': ['dya'], 'ぢぃ': ['dyi'], 'ぢゅ': ['dyu'], 'ぢぇ': ['dye'], 'ぢょ': ['dyo'],
+  'びゃ': ['bya'], 'びぃ': ['byi'], 'びゅ': ['byu'], 'びぇ': ['bye'], 'びょ': ['byo'],
+  'ぴゃ': ['pya'], 'ぴぃ': ['pyi'], 'ぴゅ': ['pyu'], 'ぴぇ': ['pye'], 'ぴょ': ['pyo'],
+  'ぁ': ['la', 'xa'], 'ぃ': ['li', 'xi'], 'ぅ': ['lu', 'xu'], 'ぇ': ['le', 'xe'], 'ぉ': ['lo', 'xo'],
+  'ゃ': ['lya', 'xya'], 'ゅ': ['lyu', 'xyu'], 'ょ': ['lyo', 'xyo'], 'っ': ['ltu', 'xtu'],
+  'ー': ['-'], ',': [','], '.': ['.'], '、': [','], '。': ['.'],
+  '・': ['/'], '、': [','], '。': ['.'], '・': ['/']
+};
 
   startButton.addEventListener("click", ready);
 
@@ -26,7 +63,7 @@ document.addEventListener("DOMContentLoaded", function() {
     scoredisElement.innerHTML = "";
     startButton.style.visibility = "hidden";
     var readytimer = setInterval(function() {
-      countElement.innerHTML = readytime;
+      countElement.innerHTML =  readytime;
       readytime--;
       if (readytime < 0) {
         clearInterval(readytimer);
@@ -44,7 +81,7 @@ document.addEventListener("DOMContentLoaded", function() {
     var gametimer = setInterval(function() {
       countElement.innerHTML = "残り時間：" + time_remaining;
       time_remaining--;
-      if (time_remaining <= 0) {
+      if (time_remaining < 0) {
         clearInterval(gametimer);
         finish();
       }
@@ -52,37 +89,39 @@ document.addEventListener("DOMContentLoaded", function() {
   }
 
   function wordDisplay() {
-    random = Math.floor(Math.random() * wordlist.length);
-    wordElement.innerHTML = wordlist[random];
-    japaneseElement.innerHTML = wordlistJapanese[random];
+    currentQuestionIndex = Math.floor(Math.random() * questions.length);
+    wordElement.innerHTML = questions[currentQuestionIndex];
+    japaneseElement.innerHTML = translations[currentQuestionIndex];
+    char_num = 0;  // char_num の初期化
     charInsort();
+}
+
+function charInsort() {
+  word_char = questions[currentQuestionIndex].charAt(char_num);
+
+  if (word_char === 'si') {
+    word_char = 'shi';
+  }
+  else if (word_char === 'ti') {
+    word_char = 'chi';
   }
 
-  function charInsort() {
-    word_char = wordlist[random].charAt(char_num);
-  }
-
-  document.addEventListener("keydown", function(e) {
-    var keyStr = String.fromCharCode(e.keyCode).toLowerCase();
-    if (keyStr == word_char) {
-      correctaudio.pause();
-      correctaudio.currentTime = 0;
-      correctaudio.play();
-      wordElement.innerHTML = "<span style='color: red;'>" + wordlist[random].substr(0, char_num + 1) + "</span>" + wordlist[random].substr(char_num + 1);
+}
+document.addEventListener("keydown", function(e) {
+  var keyStr = e.key.toLowerCase();
+  if (keyStr == word_char) {
+      wordElement.innerHTML = "<span style='color: red;'>" + questions[currentQuestionIndex].substr(0, char_num + 1) + "</span>" + questions[currentQuestionIndex].substr(char_num + 1);
       char_num++;
-      if (wordlist[random].charAt(char_num) === " ") {
+      if (questions[currentQuestionIndex].charAt(char_num) === " ") {
         char_num++;
       }
       correct++;
       charInsort();
     } else {
-      missaudio.pause();
-      missaudio.currentTime = 0;
-      missaudio.play();
       mistake++;
     }
 
-    if (char_num == wordlist[random].length) {
+    if (char_num == questions[currentQuestionIndex].length) {
       char_num = 0;
       wordDisplay();
     }
@@ -96,7 +135,22 @@ document.addEventListener("DOMContentLoaded", function() {
     japaneseElement.innerHTML = "";
     startButton.style.visibility = "visible";
     word_char = 0;
-    random = 0;
     char_num = 0;
   }
+
+  // JSONファイルから問題文と日本語訳を読み込む
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "/questions.json", true);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      var data = JSON.parse(xhr.responseText);
+      questions = data.questions.map(function(question) {
+        return question.text;
+      });
+      translations = data.questions.map(function(question) {
+        return question.translation;
+      });
+    }
+  };
+  xhr.send();
 });
