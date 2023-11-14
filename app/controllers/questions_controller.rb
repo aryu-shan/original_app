d87e2208578f7868434ef23b7760bf6c806ce1b0
aryu-shan <145306193+aryu-shan@users.noreply.github.com>
2023-11-14 14:13:10 +0900

    local設定修正

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,35 +1,5 @@
 class QuestionsController < ApplicationController
-  before_action :load_questions, only: [:index, :evaluate_answer]
-  before_action :initialize_score, only: [:index]
-
   def index
-    @question = @questions.first
-  end
-
-  def evaluate_answer
-    user_answer = params[:user_answer] # フォームからのユーザーの回答を取得する仮定
-    correct_answer = @question.correct_answer # 正解の回答を取得する仮定
-  
-    # ユーザーの回答が正解と一致する場合
-    if user_answer == correct_answer
-      @score += 1 # スコアを加算する
-    end
-  
-    # 次の質問をランダムに取得して表示する
-    @questions = @questions.where.not(id: @question.id).order("RANDOM()").limit(9)
-    @question = @questions.first
-    render :index
-  end
-
-  private
-
-  def load_questions
-    @questions = Question.order("RANDOM()").limit(10)
-  end
-
-  def initialize_score
-    @score = 0
   end
 end
 
-#"RANDOM()"のみRANDは使用不可
